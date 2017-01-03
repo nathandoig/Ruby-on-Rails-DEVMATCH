ccdd25e21a28507c37fd0dfe25e451458ca393ba
Nathan Doig <nathan_doig@hotmail.com>
2017-01-03 17:58:32 +0000

    Cleaned up Contact Us route and added Sign Up Navbar Link (without route)

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      flash[:success] = "Thanks for your feedback!"
+      flash[:success] = "Thank you for your feedback!"
       redirect_to new_contact_path
     else
       flash[:danger] = @contact.errors.full_messages.join(", ")
