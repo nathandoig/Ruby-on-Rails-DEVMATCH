86bbde4638f9f6af56e626fa207723713cd3bd4d
Nathan Doig <nathan_doig@hotmail.com>
2017-01-12 19:28:02 +0000

    Added server side code for creating subscriptions for premium users**

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
          
   belongs_to :plan
   
-  attr_accessor :stripe_card_token
+ attr_accessor :stripe_card_token
   def save_with_subscription
     if valid?
-      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
-      self.stripe_customer_token = customer.id
-      save!
+    customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
+    self.stripe_customer_token = customer.id
+    save!
     end
   end
 end
