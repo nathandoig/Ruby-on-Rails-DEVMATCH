120385695bcc0aa1777f2e259262e5598867f4f2
Nathan Doig <nathan_doig@hotmail.com>
2017-01-17 16:27:58 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
   # Get to /users/:user_id/profile/new
   def new
     # Render blank profile details form to fill out
+    @profile = Profile.new
   end
 end
