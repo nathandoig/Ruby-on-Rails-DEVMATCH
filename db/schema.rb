9b1594aa6270104f0af55e5858464344ae38746c
Nathan Doig <nathan_doig@hotmail.com>
2017-01-18 19:37:25 +0000

    Added avatar column to profiles table and dropped and remigrated db after error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118174656) do
+ActiveRecord::Schema.define(version: 20170118192905) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
