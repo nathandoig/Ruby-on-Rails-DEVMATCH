8b864fac0ef0fb8987b8f66436f7808d2ef4100f
Nathan Doig <nathan_doig@hotmail.com>
2017-01-26 05:51:52 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#index'
+  root to: 'pages#home'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users do
     resource :profile
